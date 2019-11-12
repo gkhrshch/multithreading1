3de06615ea5ef389d2975c50e8088636f10764c5
gkhrshch <george.khruschov@gmail.com>
2019-11-12 05:51:42 +0200

    Solution updated

--- src/main/java/ThreadRacing.java
+++ src/main/java/ThreadRacing.java
@@ -5,15 +5,16 @@ public class ThreadRacing {
     public static void main(String[] args) {
 
         ThreadR demo1 = new ThreadR();
-        long start = System.currentTimeMillis();
+        long begin = System.currentTimeMillis();
         demo1.run();
         long end = System.currentTimeMillis();
-        float time = end - start;
+        float time = end - begin;
         System.out.println("Thread implements Runnable execution time: " + time + "milliseconds");
         ThreadT demo2 = new ThreadT();
-        start = System.currentTimeMillis();
+        begin = System.currentTimeMillis();
         demo2.run();
         end = System.currentTimeMillis();
+        time = end - begin;
         System.out.println("Thread extends Thread execution time: " + time + "milliseconds");
     }
 }
